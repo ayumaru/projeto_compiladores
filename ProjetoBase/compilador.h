384baa77d26afc09d204c19ae214be33ed3dd960
Ayumaru <marllonwes@hotmail.com>
2021-05-12 16:10:34 -0300

    falta dar make para ver se existem erros

--- ProjetoBase/compilador.h
+++ ProjetoBase/compilador.h
@@ -7,7 +7,7 @@
  *
  * -------------------------------------------------------------------
  *
- * Tipos, prot�tipos e vari�veis globais do compilador (via extern)
+ * Tipos, prototipos e variaveis globais do compilador (via extern)
  *
  * ------------------------------------------------------------------- */
 
@@ -18,12 +18,18 @@ typedef enum simbolos {
   simb_identificador, simb_numero,
   simb_ponto, simb_virgula, simb_ponto_e_virgula, simb_dois_pontos,
   simb_atribuicao, simb_abre_parenteses, simb_fecha_parenteses,
+  simb_abre_colchetes, simb_fecha_colchetes, simb_abre_chaves, simb_fecha_chaves,
+  simb_maior_que, simb_maior_igual, simb_menor_que, simb_menor_igual,
+  simb_igual, simb_diferente, simb_soma, simb_subtracao, simb_multiplicacao,
+  simb_divisao, simb_and, simb_or, simb_label, simb_type, simb_array,
+  simb_of, simb_procedure, simb_function, simb_if, simb_else,
+  simb_while, simb_do, simb_not
 } simbolos;
 
 
 
 /* -------------------------------------------------------------------
- * vari�veis globais
+ * variaveis globais
  * ------------------------------------------------------------------- */
 
 extern simbolos simbolo, relacao;
