e966d1fe4ebf02fc99c11ae3dca65855a4792a7c
Ayumaru <marllonwes@hotmail.com>
2021-07-22 18:21:26 -0300

    funções da pilha, com erro de ligação em compilação

--- ProjetoBase/pilha.c
+++ ProjetoBase/pilha.c
@@ -1,7 +1,9 @@
 /*
 # PARA REMOVER DEPOIS: nao entendi direito o funcionamento dessa
+# melhorar a funcao error_handler com o nome da pilha que deu erro
 */
 
+#include "debug.h"
 #include "pilha.h"
 #include "compilador.h"
 #include <stdlib.h>
@@ -22,7 +24,7 @@ void insere_elemento_tipo_pilha(pilha_tipo_enum *pilha_tipo, tipos_enum tipo)
 
     nodo->tipo = tipo;
     nodo->prox = NULL; // ultimo elemento da pilha
-    nodo->tam+=1;
+    pilha_tipo->tam+=1;
     
     if(pilha_tipo->tam == 1)
         pilha_tipo->ultimo = nodo;
@@ -39,7 +41,7 @@ int remove_elemento_tipo_pilha(pilha_tipo_enum *pilha_tipo)
 
     nodo = pilha_tipo->ultimo;
     if( (nodo == NULL) || (pilha_tipo->tam == 0))
-        error_handler("[ERRO] a pilha nao possui elementos para serem removidos");
+        error_handler("[ERRO] a pilha nao possui elementos para serem removidos\n");
 
     pilha_tipo->ultimo = pilha_tipo->ultimo->prox;
     pilha_tipo->tam-=1;
@@ -80,7 +82,7 @@ int verificador_tipos(pilha_tipo_enum *pilha_tipo, tipos_enum esperado, tipos_en
     if ( (var1 == esperado) && ( var2 == esperado) )
         insere_elemento_tipo_pilha(pilha_tipo, resultado);
     else
-        error_handler("Tipo de dados nao sao iguais.");
+        error_handler("Tipo de dados nao sao iguais.\n");
 }
 
 int verifica_atributo_tipos(pilha_tipo_enum *pilha_tipo)
@@ -93,7 +95,7 @@ int verifica_atributo_tipos(pilha_tipo_enum *pilha_tipo)
     remove_elemento_tipo_pilha(pilha_tipo);
 
     if (var1 != var2)
-        error_handler("Tipos de dados nao sao iguais.");
+        error_handler("Tipos de dados nao sao iguais.\n");
 }
 
 int verifica_tipos_expressao(pilha_tipo_enum *pilha_tipo, tipos_enum esperado)
@@ -102,7 +104,7 @@ int verifica_tipos_expressao(pilha_tipo_enum *pilha_tipo, tipos_enum esperado)
     var1 = pilha_tipo->ultimo->tipo;
     remove_elemento_tipo_pilha(pilha_tipo);
     if (var1 != esperado)
-        error_handler("Tipos de dados nao sao iguais.");
+        error_handler("Tipos de dados nao sao iguais. \n");
 }
 
 void andar_pilha_tipos(pilha_tipo_enum *pilha_tipo) // parece ser func de debug
@@ -120,3 +122,229 @@ void andar_pilha_tipos(pilha_tipo_enum *pilha_tipo) // parece ser func de debug
 /* -------------------- FIM FUNCOES DA PILHA DE TIPOS -------------------- */
 
 /* -------------------- COMECO FUNCOES DA PILHA DE ROTULOS -------------------- */
+
+char *insere_rotulo(pilha_rotulo *p_rotulo)
+{
+    nodo_rotulo *nodo;
+
+    nodo = malloc(sizeof(nodo_rotulo));
+    nodo->prox = NULL;
+    // vai criando os rotulos sobre demanda
+    sprintf(nodo->rotulo,"R%02d", p_rotulo->atual);
+    p_rotulo->atual+=1;
+
+    if (p_rotulo->atual == 1)
+        p_rotulo->ultimo = nodo;
+    else
+    {
+        nodo->prox = p_rotulo->ultimo;
+        p_rotulo->ultimo = nodo;
+    }
+
+    return nodo->rotulo;
+}
+
+int remove_rotulo(pilha_rotulo *p_rotulo)
+{
+    nodo_rotulo *nodo;
+    nodo = p_rotulo->ultimo;
+
+    if ((nodo == NULL) || (p_rotulo->atual == 0))
+        error_handler("Pilha de rotulos sem elemento. Erro de acesso.\n");
+    
+    p_rotulo->ultimo = p_rotulo->ultimo->prox;
+    free(nodo);
+}
+
+void destroi_pilha_rotulo(pilha_rotulo *p_rotulo)
+{
+    nodo_rotulo *nodo, *tmp;
+
+    nodo = p_rotulo->ultimo;
+    while(nodo != NULL)
+    {
+        tmp = nodo;
+        nodo = nodo->prox;
+        free(tmp);
+    }
+
+    free(p_rotulo);
+    p_rotulo = NULL;
+}
+
+void andar_pilha_rotulo(pilha_rotulo *p_rotulo) // para debug?
+{
+    nodo_rotulo *nodo;
+    nodo = p_rotulo->ultimo;
+
+    for(int nivel = 0; (nodo != NULL); nivel++)
+    {
+        debug("[DEBUG]tipo = [%s], nivel = [%d]\n",nodo->rotulo, nivel);
+        nodo = nodo->prox;
+        debug("[warning] All nodes from stack should have been consumed\n");
+    }
+}
+
+/* -------------------- FIM FUNCOES DA PILHA DE ROTULOS -------------------- */
+
+/* -------------------- COMECO FUNCOES DA PILHA DE DESLOCAMENTO -------------------- */
+
+void pilha_insere_deslocamento(pilha_deslocamento *p_deslocamento, int quantidade)
+{
+    nodo_deslocamento *nodo;
+    nodo = malloc(sizeof(nodo_deslocamento));
+    nodo->prox = NULL;
+    nodo->desloc = quantidade;
+
+    p_deslocamento->nv_lexico+=1;
+
+    if (p_deslocamento->nv_lexico == 1)
+        p_deslocamento->ultimo = nodo;
+    else
+    {
+        nodo->prox = p_deslocamento->ultimo;
+        p_deslocamento->ultimo = nodo;
+    }
+}
+
+int pilha_remove_deslocamento(pilha_deslocamento *p_deslocamento)
+{
+    nodo_deslocamento *nodo;
+    nodo = p_deslocamento->ultimo;
+
+    if( (nodo == NULL) || (p_deslocamento->nv_lexico == 0))
+        error_handler("[ERRO] a pilha deslocamento nao possui elementos para serem removidos\n");
+
+    p_deslocamento->ultimo = p_deslocamento->ultimo->prox;
+    p_deslocamento->nv_lexico-=1;
+
+    free(nodo);
+}
+
+void destroi_pilha_deslocamento(pilha_deslocamento *p_deslocamento)
+{
+    nodo_deslocamento *nodo, *tmp;
+
+    nodo = p_deslocamento->ultimo;
+    while(nodo != NULL)
+    {
+        tmp = nodo;
+        nodo = nodo->prox;
+        free(tmp);
+    }
+
+    free(p_deslocamento);
+    p_deslocamento = NULL;
+}
+
+int andar_pilha_deslocamento(pilha_deslocamento *p_deslocamento) // debug
+{
+    nodo_deslocamento *nodo;
+    nodo = p_deslocamento->ultimo;
+
+    for(int nivel = 0; (nodo != NULL); nivel++)
+    {
+        debug("[DEBUG]tipo = [%s], nivel = [%d]\n",nodo->desloc, nivel);
+        nodo = nodo->prox;
+        debug("[warning] All nodes from stack should have been consumed\n");
+    }
+}
+
+/* -------------------- FIM FUNCOES DA PILHA DE DESLOCAMENTO -------------------- */
+
+
+/* -------------------- COMECO FUNCOES DA PILHA DE PROCEDIMENTO C/ ROTULO -------------------- */
+char *insere_pilha_procedimento_rotulo(pilha_procedimento_rotulo *p_procrot)
+{
+    nodo_procedimento_rotulo *nodo;
+
+    nodo = malloc(sizeof(nodo_procedimento_rotulo));
+    nodo->prox = NULL;
+
+    sprintf(nodo->rotulo,"P%02d", p_procrot->atual); // da um rotulo para o atual
+
+    p_procrot->atual+=1;
+
+    if (p_procrot->atual == 1)
+        p_procrot->ultimo = nodo;
+    else
+    {
+        nodo->prox = p_procrot->ultimo;
+        p_procrot->ultimo = nodo;
+    }
+
+    return nodo->rotulo;
+}
+
+void destroi_pilha_procedimento_rotulo(pilha_procedimento_rotulo *p_procrot)
+{
+    nodo_procedimento_rotulo *nodo, *tmp;
+
+    nodo = p_procrot->ultimo;
+    while(nodo != NULL)
+    {
+        tmp = nodo;
+        nodo = nodo->prox;
+        free(tmp);
+    }
+
+    free(p_procrot);
+    p_procrot = NULL;
+}
+
+/* -------------------- FIM FUNCOES DA PILHA DE PROCEDIMENTO C/ ROTULO -------------------- */
+
+/* -------------------- COMECO FUNCOES DA PILHA DE PROCEDIMENTO C/ SIMBOLO -------------------- */
+
+void insere_pilha_simb(pilha_proc_simb *p_proc_sb, tab_simbolo *simb)
+{
+    nodo_proc_simb *nodo;
+    
+    nodo = malloc(sizeof(nodo_procedimento_rotulo));
+    nodo->prox = NULL;
+
+    nodo->simbolo = simb;
+
+    p_proc_sb->atual+=1;
+
+    if (p_proc_sb->atual == 1)
+        p_proc_sb->ultimo = nodo;
+    else
+    {
+        nodo->prox = p_proc_sb->ultimo;
+        p_proc_sb->ultimo = nodo;
+    }
+
+}
+
+int remove_pilha_simb(pilha_proc_simb *p_proc_sb)
+{
+    nodo_proc_simb *nodo;
+    nodo = p_proc_sb->ultimo;
+
+    if( (nodo == NULL) || (p_proc_sb->atual == 0))
+        error_handler("[ERRO] a pilha deslocamento nao possui elementos para serem removidos\n");
+
+    p_proc_sb->ultimo = p_proc_sb->ultimo->prox;
+    p_proc_sb->atual-=1;
+
+    free(nodo);
+}
+
+void destroi_pilha_simb(pilha_proc_simb *p_proc_sb)
+{
+    nodo_proc_simb *nodo, *tmp;
+
+    nodo = p_proc_sb->ultimo;
+    while(nodo != NULL)
+    {
+        tmp = nodo;
+        nodo = nodo->prox;
+        free(tmp);
+    }
+
+    free(p_proc_sb);
+    p_proc_sb = NULL;
+}
+
+/* -------------------- FIM FUNCOES DA PILHA DE PROCEDIMENTO C/ ROTULO -------------------- */
--- ProjetoBase/pilha.h
+++ ProjetoBase/pilha.h
@@ -19,7 +19,7 @@ typedef struct pilha_tipo_enum{
 
 // funcoes para pilha de tipos
 void insere_elemento_tipo_pilha(pilha_tipo_enum *pilha_tipo, tipos_enum tipo);
-int remove_elemento_tipo_pilha( nodo_tipos_enum *pilha_tipo);
+int remove_elemento_tipo_pilha(pilha_tipo_enum*pilha_tipo);
 
 void destruir_pilha_tipos(pilha_tipo_enum *pilha_tipo); 
 
@@ -32,5 +32,83 @@ void andar_pilha_tipos(pilha_tipo_enum *pilha_tipo); // parece servir para debug
 /* -------------------- FIM PILHA DE TIPOS -------------------- */
 
 /* -------------------- COMECO PILHA DE ROTULOS -------------------- */
+
+typedef struct nodo_rotulo{
+    char rotulo[TAM_ROTULO];
+    struct nodo_rotulo *prox;
+} nodo_rotulo;
+
+typedef struct pilha_rotulo{
+    struct nodo_rotulo *ultimo;
+    int atual;
+} pilha_rotulo;
+
+char *insere_rotulo(pilha_rotulo *p_rotulo);
+int remove_rotulo(pilha_rotulo *p_rotulo);
+
+void destroi_pilha_rotulo(pilha_rotulo *p_rotulo);
+
+void andar_pilha_rotulo(pilha_rotulo *p_rotulo); // serve de debug
+
+
+/* -------------------- FIM PILHA DE ROTULOS -------------------- */
+
+
+/* -------------------- COMECO PILHA DE DESLOCAMENTO -------------------- */
+
+typedef struct nodo_deslocamento {
+    int desloc;
+    struct nodo_deslocamento *prox;
+} nodo_deslocamento;
+
+typedef struct pilha_deslocamento{
+    struct nodo_deslocamento *ultimo;
+    int nv_lexico;
+} pilha_deslocamento;
+
+void pilha_insere_deslocamento(pilha_deslocamento *p_deslocamento, int quantidade);
+int pilha_remove_deslocamento(pilha_deslocamento *p_deslocamento);
+
+void destroi_pilha_deslocamento(pilha_deslocamento *p_deslocamento);
+
+int andar_pilha_deslocamento(pilha_deslocamento *p_deslocamento); // serve de debug
+
+
+/* -------------------- FIM PILHA DE DESLOCAMENTO -------------------- */
+
+/* -------------------- COMECO PILHA DE PROCEDIMENTOS ROTULO -------------------- */
+
+typedef struct nodo_procedimento_rotulo{
+    struct nodo_procedimento_rotulo *prox;
+    char rotulo[TAM_ROTULO];
+} nodo_procedimento_rotulo;
+
+typedef struct pilha_procedimento_rotulo{
+    int atual;
+    struct nodo_procedimento_rotulo *ultimo;
+} pilha_procedimento_rotulo;
+
+char *insere_pilha_procedimento_rotulo(pilha_procedimento_rotulo *p_procrot);
+void destroi_pilha_procedimento_rotulo(pilha_procedimento_rotulo *p_procrot);
+
+/* -------------------- FIM PILHA DE PROCEDIMENTOS ROTULO -------------------- */
+
+/* -------------------- COMECO PILHA DE PROCEDIMENTOS SIMBOLOS -------------------- */
+
+typedef struct nodo_proc_simb{
+    struct nodo_proc_simb *prox;
+    tab_simbolo *simbolo;
+} nodo_proc_simb;
+
+typedef struct pilha_proc_simb{
+    nodo_proc_simb *ultimo;
+    int atual;
+} pilha_proc_simb;
+
+void insere_pilha_simb(pilha_proc_simb *p_proc_sb, tab_simbolo *simb);
+int remove_pilha_simb(pilha_proc_simb *p_proc_sb);
+
+void destroi_pilha_simb(pilha_proc_simb *p_proc_sb);
+
 
 #endif
